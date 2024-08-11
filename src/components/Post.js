4a515601c9020263d4980ab827a5879d60aa8431
oliverwigren <oliver.wigren@hotmail.com>
2024-08-11 12:50:16 +0200

    post

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,7 @@ import style from '../style/Post.module.css'
 function Post({postContent}) {
     return (
         <div className={style.div}>
-            <p>{postContent}</p>
+            <p className={style.text}>{postContent}</p>
         </div>
     )
 }
