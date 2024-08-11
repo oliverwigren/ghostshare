d19282208947ab84d63cc89079ea666c199bf0c1
oliverwigren <oliver.wigren@hotmail.com>
2024-08-11 12:58:30 +0200

    links

--- src/components/HeaderArea.js
+++ src/components/HeaderArea.js
@@ -2,11 +2,11 @@ import React from "react";
 import styles from '../style/HeaderArea.module.css'
 import Links from './Links';
 
-function HeaderArea ({setShowCrP}) {
+function HeaderArea () {
     return (
         <div className={styles.div}>
             <h1 className={styles.h1}>GHOSTSHARE</h1>
-            <Links setShowCrP={setShowCrP} />
+            <Links />
         </div>
     )
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -3,17 +3,18 @@ import style from "../style/Links.module.css";
 import { NavLink } from "react-router-dom";
 
 function Links() {
+
   return (
     <div className={style.div}>
       {/* <button className={style.button}>Feed</button> */}
-      <NavLink to="/" className={style.button}>
+      <NavLink to="/" className={({isActive}) => isActive ? style.button + ' ' + style.active : style.button}>
         Feed
       </NavLink>
-      <NavLink to="/profile" className={style.button}>
+      <NavLink to="/profile" className={({isActive}) => isActive ? style.button + ' ' + style.active : style.button}>
         Profile
       </NavLink>
       {/* <button className={style.button} onClick={() => setShowCrP(true)} >Post</button> */}
-      <NavLink to="/post" className={style.button}>
+      <NavLink to="/post" className={({isActive}) => isActive ? style.button + ' ' + style.active : style.button}>
         Post
       </NavLink>
     </div>
