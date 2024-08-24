952e1614b63c117625cfaf88aa904a21c030790a
oliverwigren <oliver.wigren@hotmail.com>
2024-08-24 15:14:05 +0200

    create account component

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,28 +1,57 @@
-import React from "react";
-import { addUser } from './components/Firebase';
+import React, { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "./firebase/auth";
 
 export function CreateAccount() {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  const auth = getAuth();
 
-        // addUser({
-        //     username:
-        // })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    return (
-       <section>
-        <h1></h1>
-        <p></p>
+  createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // ...
+        console.log(user + ' signed up!')
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode + ' ' + errorMessage)
+      });
+  };
 
-         <form onSubmit={handleSubmit}>
-            <label htmlFor="username" >Username</label>
-            <input name="username" type="username" placeholder="username" id="username" autoComplete="off" />
-            <label htmlFor="password">Password</label>
-            <input name="password" type="password" placeholder="password" id="password" autoComplete="off" />
-            <button type="submit">Sign up </button>
-        </form>   
-       </section>
-    )
+  return (
+    <section>
+      <h1></h1>
+      <p></p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+          id="email"
+          autoComplete="off"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+          id="password"
+          autoComplete="off"
+        />
+        <button type="submit">Sign up </button>
+      </form>
+    </section>
+  );
 }
