fca21641fbf4323191e318b6490e5d4f422cc4f6
oliverwigren <oliver.wigren@hotmail.com>
2024-09-03 13:55:11 +0200

    fixed error

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -25,10 +25,10 @@ export default db;*/
 
 //import firebase from "firebase";
 //import "firebase/firestore";
-//import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc, addDoc, collection, orderBy, query, limit, Timestamp } from "firebase/firestore";
 // getDocs, onSnapshot, QuerySnapshot, getDoc
-/*const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyCFKQqsvFcQ20np8g1yXp6ewnQkoKe3kn4",
     authDomain: "ghostshare-f234e.firebaseapp.com",
     projectId: "ghostshare-f234e",
@@ -36,10 +36,11 @@ import { getFirestore, doc, setDoc, addDoc, collection, orderBy, query, limit, T
     messagingSenderId: "614794423461",
     appId: "1:614794423461:web:c5e78d60db70b8d498714c",
     measurementId: "G-C8N5KZ1RD5"
-  };*/
+  };
 
 // Initialize Firebase
-//const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line
+const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore();
