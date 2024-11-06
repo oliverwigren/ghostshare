c61d6e314faf9ad122ccaa12139877579bd80ab1
oliverwigren <oliver.wigren@hotmail.com>
2024-11-06 17:50:29 +0100

    firebaseconfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCFKQqsvFcQ20np8g1yXp6ewnQkoKe3kn4",
+    authDomain: "ghostshare-f234e.firebaseapp.com",
+    projectId: "ghostshare-f234e",
+    storageBucket: "ghostshare-f234e.appspot.com",
+    messagingSenderId: "614794423461",
+    appId: "1:614794423461:web:c5e78d60db70b8d498714c",
+    measurementId: "G-C8N5KZ1RD5"
+  };
